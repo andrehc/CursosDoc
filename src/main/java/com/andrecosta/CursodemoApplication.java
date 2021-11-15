2e369123b3af20b324a207debc0a350dc2c50aff
Andre Costa <andre.hcosta@hotmail.com>
2021-11-15 18:34:08 -0300

    Criando projeto e primeira classe

---
+++ src/main/java/com/andrecosta/CursodemoApplication.java
@@ -0,0 +1,13 @@
+package com.andrecosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursodemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrecosta/demo/CursodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrecosta.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
